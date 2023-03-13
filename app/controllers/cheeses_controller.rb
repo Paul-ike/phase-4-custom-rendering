ca5ce42a5b13103b9ff77867f9bd05423df024c0
Paul <paul.ngugi@student.moringaschool.com>
2023-03-13 22:07:49 +0300

    done

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -12,6 +12,7 @@ class CheesesController < ApplicationController
     if cheese
       render json: cheese, except: [:created_at, :updated_at], methods: [:summary]
     else
+      #status: :not_found will produce a 404 status code
       render json: { error: 'Cheese not found' }, status: :not_found
     end
   end
